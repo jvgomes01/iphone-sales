9b821417577f87174448acfb9a893808ae1e4341
Joao Victor Gomes <61714929+jvgomes01@users.noreply.github.com>
2024-02-23 12:35:43 -0300

    Add files via upload
    imgs add

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const img = document.querySelector("#main img")
+const imgPickerGreen = document.querySelector("#green")
+const imgPickerSilver = document.querySelector("#silver")
+const imgPickerGolden = document.querySelector("#golden")
+const imgPickerGrafite = document.querySelector("#grafite")
+const imgPickerBlue = document.querySelector("#blue")
+const imgPicker = document.querySelector("#image-picker")
+
+function alterarImg() {
+    
+}
+
+imgPicker.addEventListener("click", () => {
+        alterarImg()
+    })
